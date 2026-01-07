f46e88564c40a8cf7880efcb7c9f2ed329f02b48
careercanvas99 <careercanvas99@gmail.com>
2026-01-07 11:34:31 +0530

    SQL TRANSACTION: Commit database.sql

--- crm-data/database.sql
+++ crm-data/database.sql
@@ -1,5 +1,5 @@
 -- Drivebuddy CRM SQL Dump
--- Generated: 2026-01-07T06:04:27.208Z
+-- Generated: 2026-01-07T06:04:30.994Z
 
 CREATE TABLE IF NOT EXISTS users (id TEXT PRIMARY KEY, username TEXT, role TEXT, password TEXT, phone TEXT);
 CREATE TABLE IF NOT EXISTS drivers (id TEXT PRIMARY KEY, name TEXT, phone TEXT, licenseNumber TEXT, licenseExpiry TEXT, status TEXT);
@@ -9,9 +9,9 @@ CREATE TABLE IF NOT EXISTS trips (id TEXT PRIMARY KEY, customerId TEXT, driverId
 -- Users Data
 INSERT OR REPLACE INTO users VALUES ('USR1767765448816', 'shravan', 'DRIVER', '2211', '');
 INSERT OR REPLACE INTO users VALUES ('USR1767765820855', 'gopal', 'CUSTOMER', '1122', '6300665549');
-INSERT OR REPLACE INTO users VALUES ('USR1767765865472', 'shravann', 'DRIVER', '1122', '9876543210');
 
 -- Drivers Data
+INSERT OR REPLACE INTO drivers VALUES ('USR1767765865472', 'shravann', '9876543210', 'PENDING_REGISTRATION', '2027-01-07', 'OFFLINE');
 
 -- Customers Data
 
