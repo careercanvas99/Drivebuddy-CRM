42813cfbcbf385c9a6b64c6aa5d5583b559848cc
careercanvas99 <careercanvas99@gmail.com>
2026-01-07 11:32:59 +0530

    SQL TRANSACTION: Commit database.sql

--- crm-data/database.sql
+++ crm-data/database.sql
@@ -1,5 +1,5 @@
 -- Drivebuddy CRM SQL Dump
--- Generated: 2026-01-07T05:57:30.477Z
+-- Generated: 2026-01-07T06:02:57.946Z
 
 CREATE TABLE IF NOT EXISTS users (id TEXT PRIMARY KEY, username TEXT, role TEXT, password TEXT, phone TEXT);
 CREATE TABLE IF NOT EXISTS drivers (id TEXT PRIMARY KEY, name TEXT, phone TEXT, licenseNumber TEXT, licenseExpiry TEXT, status TEXT);
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS customers (id TEXT PRIMARY KEY, name TEXT, phone TEXT
 CREATE TABLE IF NOT EXISTS trips (id TEXT PRIMARY KEY, customerId TEXT, driverId TEXT, status TEXT, pickup TEXT, "drop" TEXT, dateTime TEXT, basePrice REAL, finalPrice REAL);
 
 -- Users Data
-INSERT OR REPLACE INTO users VALUES ('USR1767765448816', 'shravan', 'DRIVER', 'welcome123', '');
+INSERT OR REPLACE INTO users VALUES ('USR1767765448816', 'shravan', 'DRIVER', '2211', '');
 
 -- Drivers Data
 
